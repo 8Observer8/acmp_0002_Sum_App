3f72ca3db031d06264a52508e52fd0538eb83e8a
Ivan <8observer8@gmail.com>
2013-05-11 23:41:30 +0400

    The Sum from 1 to n.

---
+++ src/acmp_0002_sum_app/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acmp_0002_sum_app;
+
+import org.me.sum.Sum;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Sum.sum(n));
+    }
+}
